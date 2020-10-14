b543b6f2af1a0f02e626ee515b514dd0e3f0ebaa
aravindhansekaran <48963447+aravindhansekaran@users.noreply.github.com>
2020-10-14 09:10:15 +0530

    modified url

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://aravindh-22:aravindh@22@questionnaire.jjkmu.mongodb.net/sample?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect('mongodb+srv://<username>:<password>@questionnaire.jjkmu.mongodb.net/sample?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true});
 
 // const db = mongoose.connection
 
